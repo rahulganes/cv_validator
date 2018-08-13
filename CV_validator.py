868d3ef34d728687eb3dd83f09ab3cc13dae1951
GreenHood <40782985+rahulganes@users.noreply.github.com>
2018-08-13 20:16:31 +0530

    Update CV_validator.py

--- CV_validator.py
+++ CV_validator.py
@@ -5,9 +5,14 @@
 
 
 
+
+from validate_email import validate_email
+import DNS
+
+
 from geopy.geocoders import Nominatim
 
-addresslocation = input("Enter YOur Address here :")
+addresslocation = input("Enter your Address here :")
 geolocator = Nominatim(user_agent="Curriculum_Validator")
 location = geolocator.geocode(addresslocation)
 try:
@@ -27,6 +32,32 @@ try :
         a1 = 0 
 except:    
     a1 = 0
+
+emailid = input("Enter your email-id :")    
+is_valid = validate_email(emailid,verify=True)
+if is_valid == True:
+    a3 = 1
+else:
+    a3 = 0
+    
+qual=input("Enter your qualification :")
+qual_List = ['b.tech','b.e','B.Tech','B.E']
+qualifi = qual.split()
+seting = set(qualifi)&set(qual_List)
+if seting != set():
+    a4 = 1
+else:
+    a4 = 0
+
+exp = int(input("Enter your experience(in numbers) :"))
+
+if exp>6:
+    a5 = 1
+elif exp>3:
+    a5 = 0
+else:
+    a5 = -1
+
 describeyourselves = input("Describe yourselves :")
 from textblob import TextBlob
 
@@ -38,15 +69,18 @@ elif(analyse.sentiment.polarity<0.25 )and(analyse.sentiment.polarity>-0.25):
 else:
     a2 = -1
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 from sklearn import tree
+
+dataset = pd.read_csv(r'C:\Users\rg\Desktop\ipynb\dataset\CV_dataset2.csv') #ENTER YOUR DATA SET HERE
+X = dataset.iloc[:, :-1].values #ORIGINAL DATA
+Y = dataset.iloc[:, 6].values
 
 clf = tree.DecisionTreeClassifier()
 
-X = [[0,0,-1],[0,0,0],[0,0,1],[0,1,-1],[0,1,0],[0,1,1],[1,0,-1],[1,0,0],[1,0,1],[1,1,-1],[1,1,0],[1,1,1]]
-Y = ['Highly Risky','Risky','Intermediate','Risky','Intermediate','Good','Risky','Intermediate','Good','Intermediate','Good','Excellent']
-
 clf.fit(X,Y)
 
-print("How trustworthy are you?")
-print(clf.predict([[a0,a1,a2]]))
-
+print("Verdict :") #PREDICTED RESULTS
+print(clf.predict([[a0,a1,a4,a3,a2,a5]]))
